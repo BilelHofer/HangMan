d1393fcb71416efc7115cf2fa4cddcb607912d06
BilelHofer <bilel.hofer@ik.me>
2025-05-28 13:32:24 +0200

    Ajout de l'architecture de base

---
+++ GameManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HangMan
+{
+    public class GameManager
+    {
+        private List<string> guessedLetters = new List<string>();
+
+        public List<string> GuessedLetters { get => guessedLetters; set => guessedLetters = value; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,56 @@
-﻿namespace HangHim
+﻿/*
+ * Author : Hofer Bilel
+ * Date : 21.05.2025
+ * Summary : HangMan is a game where the player has to guess a word by suggesting letters within a certain number of guesses.
+ */
+
+
+namespace HangMan
 {
     internal class Program
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Start the game
+            GameManager gameManager = new GameManager();
+
+            bool isPlaying = true;
+
+            do
+            {
+                Console.Write("Which letter do you want to guess? ");
+
+                string letter = Console.ReadLine();
+
+                // Check if the letter is valid
+                if (letter.Length != 1 || !char.IsLetter(letter[0]))
+                {
+                    DisplayError("Invalid input. Please enter a valid letter.");
+                    continue;
+                }
+
+                // Check if the letter is already guessed
+                if (gameManager.GuessedLetters.Contains(letter.ToUpper()))
+                {
+                    DisplayError("You have already guessed this letter.");
+                    continue;
+                }
+
+                                
+
+            } while (isPlaying);
+        }
+
+        /// <summary>
+        /// Display an error message in red color.
+        /// </summary>
+        /// <param name="message">the message</param>
+        public static void DisplayError(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(message);
+            Console.ResetColor();
         }
     }
 }
